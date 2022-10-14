794e42f1c5ec66fdf1536e21dae5959587d3073a
Your Name <you@example.com>
2022-10-14 23:39:59 +0000

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main- Entry point of the program
+*Return: prints 1 for success
+*/
+
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
