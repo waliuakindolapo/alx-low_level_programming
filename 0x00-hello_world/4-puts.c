3c3f1f54a0d229df88ca44ef0aa2246c8821d15e
Your Name <you@example.com>
2022-10-14 19:33:45 +0000

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - This prints the desired sentence
+*
+*return - This returns 0
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
