4c7f5c9435be280c13b2e4f6c2e7f1722f1c4f58
Your Name <you@example.com>
2022-10-14 21:17:59 +0000

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+/**
+*main- This displays the desired outcome
+*Return: This returns 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
