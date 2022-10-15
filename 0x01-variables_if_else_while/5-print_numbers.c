acf11219a46443cf01e5584f6b2d46f597059403
Your Name <you@example.com>
2022-10-15 04:57:12 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main- prints single number in base 10
+*Return: returns 0
+*/
+
+int main(void)
+{	char i;
+
+	for (i >= 0; i < 10)
+	{
+		putchar (i);
+	}
+	{
+		putchar ('\n')
+	}
+	return (0)
