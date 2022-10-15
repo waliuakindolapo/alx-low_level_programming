271db69ed19015e32302b505ad7c0a97e431923a
Your Name <you@example.com>
2022-10-15 03:46:04 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main- prints alphabets in lower case
+*Return: returns 0
+*/
+
+int main(void)
+{
+	char A;
+
+	for (A = 'a'; A <= 'z'; A++)
+	{
+		putchar(A);
+	}
+	{
+		putchar("\n");
+	}
+	return (0);
+}
