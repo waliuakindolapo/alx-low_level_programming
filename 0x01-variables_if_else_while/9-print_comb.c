a66a39beaae4a7d7aac11988fa78d2aa01dbbbfa
Your Name <you@example.com>
2022-10-16 22:43:11 +0000

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main- program enrty point
+*Return: returns 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar (num);
+	}
+	if (num == '9')
+	{
+		break;
+	}
+	else
+	{
+		putchar (',');
+		PUtchar (' ');
+	}
+	putchar ('\n');
+	return (0);
+}
