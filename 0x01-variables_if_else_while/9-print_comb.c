ffacf1368e6bcafdb22020deaf976cb3152c7d48
root <waliuakindolapo@gmail.com>
2022-10-17 13:49:55 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,20 @@
 
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = '0'; num <= '9'; num++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		putchar (num + '0');
+		putchar (n + '0');
 	}
+	if ("n +'0'" < '9')
 	{
 		putchar (',');
 	}
-	for (num < '9')
 	{
 		putchar (' ');
 	}
+	else
 	{
 		putchar ('\n');
 	}
