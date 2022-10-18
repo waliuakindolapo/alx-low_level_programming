f9a35149488108cec16a3e9a6e1cf129fd4ae92d
root <waliuakindolapo@gmail.com>
2022-10-17 17:06:58 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ *main- program entry point
+ *Return: returns 0
+ */
+
+int main(void)
+{
+	int i, k = '0', j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	for (j = k; j <= '9'; j++)
+			{
+				if (i != j)
+				{
+					putchar(i);
+					putchar(j);
+				}
+				if (i == j)
+				{
+					continue;
+				}
+				if (i == '8' && j == '9')
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		k++;
+	}
+	putchar('\n');
+	retur (0);
+}
