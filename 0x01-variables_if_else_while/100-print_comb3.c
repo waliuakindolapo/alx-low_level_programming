cf44b7d4d28838eaaf61f6599fd978ad7f2301c8
root <waliuakindolapo@gmail.com>
2022-10-17 17:18:50 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,8 +11,8 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-	for (j = k; j <= '9'; j++)
-			{
+		for (j = k; j <= '9'; j++)
+		{
 				if (i != j)
 				{
 					putchar(i);
@@ -31,7 +31,7 @@ int main(void)
 					putchar(',');
 					putchar(' ');
 				}
-			}
+		}
 		k++;
 	}
 	putchar('\n');
